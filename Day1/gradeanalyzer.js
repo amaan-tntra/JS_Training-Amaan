8e4f6053c5bbf95f9ec79ea10c940fdd51143241
Amaan-Tai <amaan.tai@tntra.io>
2025-10-24 11:17:07 +0530

    Added Function to calculate highest and lowest score

--- Day1/gradeanalyzer.js
+++ Day1/gradeanalyzer.js
@@ -1,11 +1,27 @@
 const students = [
-{ name: "Alice", score: 85 },
-{ name: "Bob", score: 92 },
-{ name: "Charlie", score: 58 },
-{ name: "David", score: 74 }
+  { name: "Alice", score: 85 },
+  { name: "Bob", score: 92 },
+  { name: "Charlie", score: 58 },
+  { name: "David", score: 74 },
 ];
 
-const average = students.reduce((acc,item) => {
-    return acc + item.score / students.length
-},0);
-console.log(average)
+// Calculate average using reduce()
+const average = students.reduce((acc, item) => {
+  return acc + item.score / students.length;
+}, 0);
+console.log("Average Score:", average);
+
+//Function to calculate highest and lowest score
+function highlow(students) {
+  const highest = students.reduce((max, value) => {
+    return value.score > max.score ? value : max;
+  });
+  const lowest = students.reduce((min, value) => {
+    return value.score < min.score ? value : min;
+  });
+  return { highest, lowest };
+}
+
+let {highest, lowest} = highlow(students);
+console.log(`Highest score: ${highest.name} (${highest.score})`);
+console.log(`Lowest score: ${lowest.name} (${lowest.score})`);
