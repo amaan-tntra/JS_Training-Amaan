0f56d3f6a211e97656c01b0b773d3523d6fdff26
Amaan-Tai <amaan.tai@tntra.io>
2025-10-24 12:55:50 +0530

    Added functions for grade distribution and retake students

--- Day1/gradeanalyzer.js
+++ Day1/gradeanalyzer.js
@@ -22,6 +22,42 @@ function highlow(students) {
   return { highest, lowest };
 }
 
-let {highest, lowest} = highlow(students);
+let { highest, lowest } = highlow(students);
 console.log(`Highest score: ${highest.name} (${highest.score})`);
 console.log(`Lowest score: ${lowest.name} (${lowest.score})`);
+
+//Function to count of students in each grade category
+function gradeDistribution(students) {
+  const grades = {
+    A: 0,
+    B: 0,
+    C: 0,
+    D: 0,
+    F: 0,
+  };
+  students.forEach((value) => {
+    let marks = value.score;
+    if (marks >= 90) grades.A++;
+    else if (marks >= 80) grades.B++;
+    else if (marks >= 70) grades.C++;
+    else if (marks >= 60) grades.D++;
+    else if (marks < 60) grades.F++;
+    else console.log("Not valid");
+  });
+
+  return grades;
+}
+let obj1 = gradeDistribution(students);
+console.log("Grade Distribution: ",obj1);
+
+// Function to list students who need to retake the exam
+function retakeStudents() {
+  const failed = [];
+  students.filter((student) => {
+    if (student.score < 60) {
+      failed.push(student.name);
+    }
+  });
+  return failed;
+}
+console.log("Students needing retake: ",retakeStudents(students));
